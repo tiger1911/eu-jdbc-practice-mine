8730afe1e64845408e4afa069a76b939cd48148a
CybertekSchool <github@cybertekschool.com>
2020-11-19 13:58:40 -0500

    Day 4

--- src/test/java/apitests/hrApiWithPath.java
+++ src/test/java/apitests/hrApiWithPath.java
@@ -6,6 +6,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import utilities.ConfigurationReader;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.baseURI;
 
 import static io.restassured.RestAssured.*;
@@ -32,8 +34,43 @@ public class hrApiWithPath {
         String firstCountryId = response.path("items.country_id[0]");
         System.out.println("firstCountryId = " + firstCountryId);
 
-        String secondCountryName = response.path("items.country_name[1]");
+        String secondCountryName = response.path("items.country_name[-1]");
         System.out.println("secondCountryName = " + secondCountryName);
+
+        String link2 =response.path("items.links[2].href[0]");
+        System.out.println("link2 = " + link2);
+
+        //get all countries
+        List<String> countryNames = response.path("items.country_name");
+        System.out.println("countryNames = " + countryNames);
+
+        //assert that all regions'ids are equal to 2
+        List<Integer> regionIDs = response.path("items.region_id");
+
+        for (int regionID : regionIDs) {
+            System.out.println(regionID);
+            assertEquals(regionID,2);
+        }
+    }
+
+    @Test
+    public void test2(){
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"job_id\":\"IT_PROG\"}")
+                .when().get("/employees");
+
+        assertEquals(response.statusCode(),200);
+        assertEquals(response.contentType(),"application/json");
+        assertTrue(response.body().asString().contains("IT_PROG"));
+
+        //makes sure we have only IT_PROG as a job_id
+        List<String> jobIDs = response.path("items.job_id");
+
+        for (String jobID : jobIDs) {
+            System.out.println("jobID = " + jobID);
+            assertEquals(jobID,"IT_PROG");
+        }
+
 
     }
 
